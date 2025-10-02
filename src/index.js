fd1ca6c0859664ebc5fc679fc6bb31bfe25f7fcb
AdityaSingh1707 <aditya.singh.41203@gmail.com>
2025-10-02 21:10:57 +0530

    utils files creation: AsyncHandler and ApiError & ApiResponse classes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 import dotenv from 'dotenv'
 import {connectDB} from './db/index.js'
+import {app} from './app.js'
 
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
+.then(() => {
+    app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+})
+.then(() => {
+    const port = process.env.PORT || 8000
+    app.listen(port, () => {
+        console.log(`Server is running at port : ${port}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!! ", err);
+})
+
+
+
+
 
 /*
 import express from 'express';
